92ef7945e75bd876952af85c052e6e083e3357fd
clearlove <1791377179@qq.com>
2022-04-17 15:21:17 +0800

    2022.4.17 15

--- assets/js/login.js
+++ assets/js/login.js
@@ -14,6 +14,8 @@ $(function() {
     //从layui中获取form对象
     //因为导入了layui,所以就可以使用layui的form对象
     var form = layui.form
+        //在layui网站的弹出层里面的msg提示找到的属性
+    var layer = layui.layer
         //通过 form.verify函数自定义校验规则
     form.verify({
         //自定义了一个叫做pwd 校验规则
@@ -26,22 +28,49 @@ $(function() {
             //4.如果判断失败，就return一个提示消息
             var pwd = $('.reg-box [name = password]').val()
             if (pwd !== value) {
-                return '两次密码输入不一致，请重新输入！'
+                return '两次密码输入不一致！'
             }
         }
     })
 
     //监听注册表单的提交事件
     $('#form_reg').on('submit', function(e) {
-        //1.阻止默认提交行为
-        e.preventDefault()
-            //2.发起ajax的post请求
-        $.post('http://www.liulongbin.top:3007/api/reguser', { uname: $('#form_reg [name="username"]').val(), password: $('#form_reg [name=password]').val() }, function(res) {
-            if (res.status !== 1) {
-                return console.log(res.message)
+            //1.阻止默认提交行为
+            e.preventDefault()
+                //2.发起ajax的post请求
+            var data = {
+                username: $('#form_reg [name="username"]').val(),
+                password: $('#form_reg [name=password]').val()
             }
-            console.log("注册成功！")
+            $.post('http://www.liulongbin.top:3007/api/reguser', data, function(res) {
+                if (res.status !== 0) {
+                    // return console.log(res.message)
+                    return layer.msg(res.message)
+                }
+                // console.log("注册成功！")
+                layer.msg('注册成功，请登录！')
+                    //模拟人为的点击事件
+                $('#link_login').click()
+            })
         })
-
+        //监听登录表带的提交事件
+    $('#form_login').submit(function(e) {
+        //阻止表单的默认提交行为
+        e.preventDefault()
+        $.ajax({
+            url: "http://www.liulongbin.top:3007/api/login",
+            method: 'post',
+            //快速获取表单中的数据，serialize就是获取数据的属性
+            data: $(this).serialize(),
+            success: (function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功！')
+                    // console.log(res.token)
+                    //跳转到后台主页
+                location.href = "./index.html"
+            })
+        })
     })
 })
